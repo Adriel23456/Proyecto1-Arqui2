43e284d261b003b96345f67d14f00c674715475c
Adriel23456 <adriel.chaves@hotmail.com>
2025-10-06 18:07:39 -0600

    Arreglo visual para que todas las instrucciones tengan el mismo tamaño

--- MainProgram/src/programs/cpu_tlp_shared_cache/views/PE2CPUView.cpp
+++ MainProgram/src/programs/cpu_tlp_shared_cache/views/PE2CPUView.cpp
@@ -22,13 +22,15 @@ void PE2CPUView::render() {
 
     m_viewer.renderWithOverlay(*m_tex, "##PE2CPU_Viewer",
         [this](const ImVec2& origin, float scale, ImDrawList* dl) {
-            constexpr float Y_IMG = 550.0f;
-            constexpr float X0_IMG = 2050.0f;
-            constexpr float DX_IMG = 1400.0f;
-            constexpr float EXTRA_BETWEEN_2_3 = 250.0f;
+            // Coordenadas en "pixeles de imagen"
+            constexpr float Y_IMG = 550.0f;     // vertical se mantiene
+            constexpr float X0_IMG = 1800.0f;    // 1er cuadro
+            constexpr float DX_IMG = 1400.0f;    // espaciamiento base
+            constexpr float EXTRA_BETWEEN_2_3 = 250.0f; // <-- extra solo entre 2º y 3º
             constexpr int   COUNT = 5;
 
-            constexpr float BOX_W_IMG = 750.0f;
+            // Tamaño MUY grande (en px de imagen), escala con la imagen
+            constexpr float BOX_W_IMG = 1200.0f;
             constexpr float BOX_H_IMG = 180.0f;
 
             const ImU32 FILL = IM_COL32(0, 0, 0, 220);
